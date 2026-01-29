db5443e774e28fdfd4e675abebc7512476e05418
shadin khan <13307041@iubat.edu>
2026-01-29 09:49:04 +0600

    set email password authentication

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,27 @@
 import express from "express";
 import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 
 const app = express();
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL ,
     credentials: true,
 }));
 
+// app.use(cors())
+
 app.use(express.json());
+
+
+
+app.all('/api/auth/*splat', toNodeHandler(auth));
+
+app.get("/", (req, res) => {
+    
+    res.send("Hello, Skill Bridge World!");
+});
 
 export default app;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,5 +6,9 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
-    
+     trustedOrigins : [process.env.FRONTED_URL!],
+    emailAndPassword : {
+        enabled : true
+    },
+
 });
